8e7b9782722faacf282a723234633d21c62ae390
Alonso <alonsoz1211@hotmail.com>
2018-03-16 14:47:50 +0100

    16.4-3-18

--- model.js
+++ model.js
@@ -1,43 +1,35 @@
 const Sequelize = require('sequelize');
+const options = {logging: false};
+const sequelize = new Sequelize("sqlite:quizzes.sqlite", options);
 
-const sequelize = new Sequelize("sqlite:quizzes.sqlite", {logging: false});
-
-const quiz = sequelize.define('quiz', {
-    question: {
-        type: Sequelize.STRING,
-        unique: {msg: "Ya existe una pregunta"},
-        validate: {notEmpty: {msg: "La respuesta no puede estar vacía"}}
-    },
-    answer: {
-        type: Sequelize.STRING,
-validate: {notEmpty: {msg: "La respuesta no puede estar vacía"}}
-    }
-});
-
+const quiz = sequelize.define( 'quiz',
+    {question: {
+            type: Sequelize.STRING,
+            unique: {msg: "Quiz already exists"},
+            validate: {notEmpty: {msg: "Question cannot be empty"}}
+        },
+        answer: {
+            type: Sequelize.STRING,
+            validate:{notEmpty: {msg: "Answer cannot be empty"}}
+        }
+    });
 
 sequelize.sync()
-    .then(() => sequelize.models.quiz.count())
-    .then(count => {
+    .then(()=> sequelize.models.quiz.count())
+    .then((count) => {
         if (!count) {
             return sequelize.models.quiz.bulkCreate([
-                { question: "Capital de Italia", answer:"Roma"},
-                { question: "Capital de Francia", answer:"Paris"},
-                { question: "Capital de España", answer:"Madrid"},
-                { question: "Capital de Portugal", answer:"Lisboa"}
+                {question: 'Capital of Spain', answer: 'Madrid', authorId: 1},
+                {question: 'Capital of France', answer: 'Paris', authorId: 1},
+                {question: 'Capital of Italy', answer: 'Rome', authorId: 2},
+                {question: 'Capital of Russia', answer: 'Moscow', authorId: 3}
             ]);
         }
     })
-    .catch(error => {
-        console.log(error);
-    });
+    .catch( err =>
+        console.log(`    ${err}`));
 
 module.exports = sequelize;
-
-
-
-
-
-
 
 
 
